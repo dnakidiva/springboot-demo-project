87f348e7023b73d6e54576e21676cf24d6c97d9b
Davies Nathan Akidiva <dnakidiva@akidivas-macbook.local>
2022-09-19 18:51:54 +0300

    Initial commit

--- src/main/java/com/example/sbdemo/SbdemoApplication.java
+++ src/main/java/com/example/sbdemo/SbdemoApplication.java
@@ -1,10 +1,13 @@
 package com.example.sbdemo;
 
+import com.example.sbdemo.customer.Customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -17,7 +20,16 @@ public class SbdemoApplication {
 	}
 
    @GetMapping
-	public List<String> hello() {
-		return List.of("Hello", "World");
+	public List<Customer> hello() {
+		return List.of(
+				new Customer(
+						23L,
+						"Natalie",
+						"natalie.adisa@gmail.com",
+						LocalDateTime.of(2000, Month.JUNE,23,7,30),
+						22
+
+				)
+		);
 	}
 }
