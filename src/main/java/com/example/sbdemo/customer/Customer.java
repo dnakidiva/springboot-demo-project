75e29c19020e606bac8ef85d478da1fbd1948992
Davies Nathan Akidiva <dnakidiva@gmail.com>
2022-10-05 22:45:33 +0300

    date calculation automation

--- src/main/java/com/example/sbdemo/customer/Customer.java
+++ src/main/java/com/example/sbdemo/customer/Customer.java
@@ -2,7 +2,8 @@ package com.example.sbdemo.customer;
 
 import javax.persistence.*;
 import java.time.LocalDate;
-import java.time.LocalDate;
+import java.time.Period;
+
 @Entity
 @Table
     public class Customer {
@@ -22,6 +23,7 @@ import java.time.LocalDate;
         private String name;
         private String email;
         private LocalDate dob;
+        @Transient
         private Integer age;
 
         public Customer() {
@@ -30,23 +32,24 @@ import java.time.LocalDate;
         public Customer(Long id,
                         String name,
                         String email,
-                        LocalDate dob,
-                        Integer age) {
+                        LocalDate dob)
+        {
             this.id = id;
             this.name = name;
             this.email = email;
             this.dob = dob;
-            this.age = age;
+
         }
 
-        public Customer(String name, String email, LocalDate dob, Integer age) {
+        public Customer(String name, String email, LocalDate dob) {
             this.name = name;
             this.email = email;
             this.dob = dob;
-            this.age = age;
+
         }
 
-        public Long getId() {
+
+    public Long getId() {
             return id;
         }
 
@@ -78,11 +81,22 @@ import java.time.LocalDate;
             this.dob = dob;
         }
 
-        public Integer getAge() {
-            return age;
+    public Integer getAge() {
+        return Period.between(this.dob,LocalDate.now()).getYears();
         }
-
-        public void setAge(Integer age) {
-            this.age = age;
-        }
+    public void setAge(Integer age) {
+        this.age = age;
     }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", dob=" + dob +
+                ", age=" + age +
+                '}';
+    }
+}
+
--- src/main/java/com/example/sbdemo/customer/CustomerConfig.java
+++ src/main/java/com/example/sbdemo/customer/CustomerConfig.java
@@ -17,15 +17,16 @@ public class CustomerConfig {
         Customer natalie = new Customer(
                 "Natalie",
                 "natalie.adisa@gmail.com",
-                LocalDate.of(2000, Month.JUNE, 23),
-                22
+                LocalDate.of(2000, Month.JUNE, 23)
         );
+
+
         Customer jamie = new Customer(
                 "Jamie",
                 "jamie.kanji@gmail.com",
-                LocalDate.of(2000, Month.AUGUST, 25),
-                22
+                LocalDate.of(2000, Month.AUGUST, 25)
         );
+
         repository.saveAll(
                 List.of(natalie,jamie)
         );
